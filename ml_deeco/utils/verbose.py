7a169377c6815dcd06fc4d98476ae5f92f46d66d
Michal Töpfer <michal.topfer@gmail.com>
2022-04-05 21:47:31 +0200

    Support saving verbose prints into a file

--- ml_deeco/utils/verbose.py
+++ ml_deeco/utils/verbose.py
@@ -1,4 +1,5 @@
 verboseLevel = 0
+file = None
 
 
 def setVerboseLevel(level):
@@ -6,7 +7,15 @@ def setVerboseLevel(level):
     verboseLevel = level
 
 
+def setVerbosePrintFile(outputFile):
+    global file
+    file = outputFile
+
+
 def verbosePrint(message: str, minVerbosity: int):
     if verboseLevel >= minVerbosity:
         print("    " * (minVerbosity - 1), end="")
         print(message)
+        if file:
+            print("    " * (minVerbosity - 1), end="", file=file)
+            print(message, file=file)
