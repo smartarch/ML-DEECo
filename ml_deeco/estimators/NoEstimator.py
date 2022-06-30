c827a379a66f57a6f9b788d69317dab307736176
Michal Töpfer <michal.topfer@gmail.com>
2022-06-30 15:20:45 +0200

    Slightly improve documentation of NoEstimator

--- ml_deeco/estimators/NoEstimator.py
+++ ml_deeco/estimators/NoEstimator.py
@@ -5,7 +5,9 @@ from ml_deeco.estimators import Estimator
 
 class NoEstimator(Estimator):
     """
-    Does not produce any training logs or outputs. Predicts 0 for each target.
+    An estimator mainly for debugging purposes (as estimate cannot be created without an estimator).
+    It does not produce any training logs (on standard output) or outputs (does not save training data).
+    Predicts 0 for each target.
     """
 
     def __init__(self, experiment, **kwargs):
